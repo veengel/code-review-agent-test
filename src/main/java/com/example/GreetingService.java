2285f677bd560fe6bc274c13d76c6a70da3981ce
Kateryna_Kanivets <kanivets.kateryna@gmail.com>
2026-09-22 21:51:06 +0300

    Added code review test cases

--- src/main/java/com/example/GreetingService.java
+++ src/main/java/com/example/GreetingService.java
@@ -1,8 +1,29 @@
 package com.example;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class GreetingService {
 
     public String greet(String name) {
-        return "Hello, " + name;
+        FileInputStream inputStream = null;
+
+        try {
+            inputStream = new FileInputStream("greeting.txt");
+        } catch (Exception e) {
+            return "Error";
+        }
+
+        int x = 7;
+
+        if (name != null) {
+            if (!name.isBlank()) {
+                if (name.length() > x) {
+                    return "Hello, " + name.toUpperCase();
+                }
+            }
+        }
+
+        return "Hello";
     }
 }
